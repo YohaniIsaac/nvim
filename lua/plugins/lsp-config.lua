c2d4257c9ef78d493cccb30615c75a228b7d5568
YohaniIsaac <yohani.tripai.yt@gmail.com>
2025-09-21 14:47:43 -0300

    fix: update version lsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -16,9 +16,29 @@ return {
     {
         "neovim/nvim-lspconfig",
         config = function()
-            local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({})
+            -- Nuevo formato usando vim.lsp.config
+            vim.lsp.config('lua_ls', {
+                cmd = { 'lua-language-server' },
+                root_markers = { '.luarc.json', '.luarc.jsonc', '.luacheckrc', '.stylua.toml', 'stylua.toml', 'selene.toml', 'selene.yml', '.git' },
+            })
+
+            vim.lsp.config('clangd', {
+                cmd = { 'clangd' },
+                root_markers = { '.clangd', '.clang-tidy', '.clang-format', 'compile_commands.json', 'compile_flags.txt', 'configure.ac', '.git' },
+            })
+
+            vim.lsp.config('cmake', {
+                cmd = { 'cmake-language-server' },
+                root_markers = { 'CMakeLists.txt', '.git' },
+            })
+
+            -- Activar los LSP servers
+            vim.lsp.enable({'lua_ls', 'clangd', 'cmake', 'cssls', 'html'})
+
+            -- Keybindings
             vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+            vim.keymap.set('n', 'gr', vim.lsp.buf.references, {})
         end
     }
 }
